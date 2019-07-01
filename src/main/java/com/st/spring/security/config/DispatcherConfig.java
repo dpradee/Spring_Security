7e38d3a9faf4465099141b69c0f53a25b131cd10
pradeep <panditpradeep1610@gmail.com>
2019-07-01 12:19:06 +0530

    initial commit

---
+++ src/main/java/com/st/spring/security/config/DispatcherConfig.java
@@ -0,0 +1,51 @@
+package com.st.spring.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.st.spring.security.controller")
+/*
+ * for @PreAuthorize to work u need to keep it in the config file where the component scan for the
+ * required classes happen. For example, here @PreAuthorize is used in controller and controllers
+ * component scan is done in this config, so this annotation has to be kept here.
+ * If the @PreAuthorize is used in service layer then @EnableGlobalMethodSecurity has to be kept in
+ * RootConfig class
+ */
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true, jsr250Enabled = true)
+public class DispatcherConfig extends WebMvcConfigurerAdapter {
+
+	/**
+	 * this is similar to <mvc:resources>
+	 */
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/img/*").addResourceLocations("/img/");
+		registry.addResourceHandler("/style/*").addResourceLocations("/style/");
+	}
+	
+	/**
+	 * similar to <mvc:default-servlet-handler>
+	 */
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+	
+	@Bean
+    public InternalResourceViewResolver getViewResolver(){
+       InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+       resolver.setPrefix("/WEB-INF/view/");
+       resolver.setSuffix(".jsp" );
+       return resolver;
+   }
+}
